2f2c396116c2f4df81b0669719297f6f86e3ac46
wolfetone <35855025+wolfetone@users.noreply.github.com>
2018-02-10 23:23:45 +0000

    Add files via upload

--- sparkiAdvancedWallAvoid.ino
+++ sparkiAdvancedWallAvoid.ino
@@ -1,33 +1,4 @@
 // Advanced Wall Avoidance 
-// servo points forward
-// move forward 
-// ping distance 
-// check for wall
-// if(wall within x distance)
-//  {
-//    stop moving forward
-//    store the distance from wall in distances array, in the 0 element
-//  } 
-// Turn servo 30 degrees left
-// ping distance
-// store distance in distances array, in element 1
-// turn servo an additional 30 degrees left
-// ping distance 
-// store distance in distances array, in element 2
-// turn servo another 30 degrees left
-// ping distance 
-// store distance in distances array, in element 3
-// return servo to center 
-// turn servo 30 degrees right 
-// ping distance 
-// store distance in distances array, in element 4
-// turn servo 30 degrees right 
-// ping distance 
-// store distance in distances array, in element 5
-// turn servo 30 degrees right 
-// ping distance 
-// store distance in distances array, in element 6
-// for(int i = 0; i <= 6; i = i + 1)
 //   
 #include <Sparki.h>
 void setup() 
